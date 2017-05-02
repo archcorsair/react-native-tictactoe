920504757e6d41c9cc77c699f96c0fb4b70cdb2a
Daniel Shneyder <archcorsair@gmail.com>
2017-05-02 01:48:27 -0700

    :new: Game fully functional

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 /* @flow */
 
-import React from 'react';
+import React from 'react'
 import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Alert,
-  Button
-} from 'react-native';
+  Container, Button, Toast, Text as NText,
+  Header, Left, Right, Body,
+  Title, Card, CardItem, Content,
+} from 'native-base'
+import {
+  StyleSheet, Text, View,
+  TouchableOpacity, Alert,
+  Vibration,
+} from 'react-native'
 
 export default class TicTacToe extends React.Component {
   constructor(props) {
@@ -18,11 +20,18 @@ export default class TicTacToe extends React.Component {
       x: [],
       o: [],
     }
-    this.resetBoard = this.resetBoard.bind(this);
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ]
+    this.resetBoard = this.resetBoard.bind(this)
+    this.updateBoard = this.updateBoard.bind(this)
+    this.checkWinner = this.checkWinner.bind(this)
+    this.displayToast = this.displayToast.bind(this)
   }
-
   renderBoard() {
-    const board = [];
+    const board = []
     for (let i = 1; i <= 9; i++) {
       board.push(
         <TouchableOpacity key={i} style={styles.box} onPress={this.tapped.bind(this, i)}>
@@ -32,10 +41,51 @@ export default class TicTacToe extends React.Component {
       </TouchableOpacity>
       )
     }
-    return board;
+    return board
+  }
+
+  updateBoard(i, player) {
+    if (i <= 3) {
+      return this.board[0][i - 1] = player
+    }
+    if (i <= 6) {
+      return this.board[1][i - 4] = player
+    }
+    if (i <= 9) {
+      return this.board[2][i - 7] = player
+    }
+  }
+
+  checkWinner(player) {
+    const b = this.board
+    // Check diags
+    const diag1 = [b[0][0], b[1][1], b[2][2]]
+    const diag2 = [b[0][2], b[1][1], b[2][0]]
+    const winner = diag1.filter(value => value === player).length === 3 || diag2.filter(value => value === player).length === 3
+    if (winner) {
+      return this.resetBoard(player)
+    }
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+      const winner = b[i].filter(value => value === player).length === 3
+      if (winner) return this.resetBoard(player)
+    }
+    // Check columns
+    const cols = [
+      [b[0][0], b[1][0], b[2][0]],
+      [b[0][1], b[1][1], b[2][1]],
+      [b[0][2], b[1][2], b[2][2]],
+    ]
+    cols.map((col) => {
+      const winner = col.filter(value => value === player).length === 3
+      if (winner) return this.resetBoard(player)
+    })
+
   }
 
   tapped(i, event) {
+    const player = this.state.currentPlayer;
+    if (this.state.x.includes(i) || this.state.o.includes(i)) return
     if (this.state.currentPlayer === 'X') {
       const x = this.state.x
       x.push(i)
@@ -45,29 +95,90 @@ export default class TicTacToe extends React.Component {
       o.push(i)
       this.setState({ o: o, currentPlayer: 'X' })
     }
-
+    this.updateBoard(i, player)
+    this.checkWinner(player)
+    if (this.state.x.length + this.state.o.length >= 9) return this.resetBoard(1)
   }
-  resetBoard() {
-    this.setState({ x: [], o: [], currentPlayer: "X" })
-    Alert.alert('Board has been reset');
+  resetBoard(player) {
+    if (this.state.x.length + this.state.o.length === 0) return
+    if (typeof player === 'object') {
+      this.displayToast(0)
+      setTimeout(() => {
+        this.setState({ x: [], o: [], currentPlayer: "X" })
+      }, 1000)
+    }
+    if (typeof player === 'number') {
+      this.displayToast(player)
+      setTimeout(() => {
+        this.setState({ x: [], o: [], currentPlayer: "X" })
+      }, 2500)
+    }
+    if (typeof player === 'string') {
+      this.displayToast(player)
+    }
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ]
+  }
+
+  displayToast(player) {
+    if (player === 1) {
+      Vibration.vibrate()
+      return Toast.show({
+        text: 'Game over!',
+        position: 'bottom',
+        buttonText: 'Okay',
+        type: 'danger',
+        duration: 2500,
+      })
+    }
+    if (player === 0) {
+      return Toast.show({
+        text: 'Starting new game',
+        position: 'bottom',
+        buttonText: 'Okay',
+        duration: 1000,
+        type: 'success',
+      })
+    }
+    if (player) {
+      Vibration.vibrate()
+      return Toast.show({
+        text: `Player ${player} wins!`,
+        position: 'bottom',
+        buttonText: 'Okay',
+        type: 'success',
+      })
+    }
   }
 
   render() {
     return (
-      <View>
-        <View style={styles.container}>
-          {this.renderBoard()}
-          <Button title="Reset" onPress={this.resetBoard} />
-          <Text>Player Turn: {this.state.currentPlayer}</Text>
-        </View>
-      </View>
-    );
+        <Container>
+          <Header>
+            <Left/>
+            <Body>
+              <Title>{`Player's Turn: ${this.state.currentPlayer}`}</Title>
+            </Body>
+            <Right />
+          </Header>
+            <View style={styles.container}>
+              {this.renderBoard()}
+              {
+                this.state.x.length + this.state.o.length > 0 &&
+                <Button bordered large onPress={this.resetBoard}><NText>New Game</NText></Button>
+              }
+            </View>
+        </Container>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: '50%',
+    marginTop: '40%',
     height: '100%',
     width: '100%',
     flexDirection: 'row',
@@ -91,6 +202,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignSelf: 'center',
     alignItems: 'center',
-    fontWeight: '900',
+    fontSize: 50,
   },
-});
+})
